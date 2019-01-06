f60d8bdb6a9d851af0cb0926766951525e200499
Noxilus <noahjgersh@icloud.com>
2019-01-06 13:30:34 -0800

    Add penguinports header and lift stage mag sensors

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,45 +11,17 @@
 
 #include <frc/smartdashboard/SmartDashboard.h>
 
-const int dio0 = 0;
-const int dio1 = 1;
-const int dio2 = 2;
-const int dio3 = 3;
-const int dio4 = 4;
-const int dio5 = 5;
-const int dio6 = 6;
-const int dio7 = 7;
-const int dio8 = 8;
-const int dio9 = 9;
-
-const int pch0 = 0;
-const int pch1 = 1;
-const int pch2 = 2;
-const int pch3 = 3;
-const int pch4 = 4;
-const int pch5 = 5;
-const int pch6 = 6;
-const int pch7 = 7;
-
-const int pcm0 = 0;
-
-const int pwm0 = 0;
-const int pwm1 = 1;
-const int pwm2 = 2;
-const int pwm3 = 3;
-const int pwm4 = 4;
-const int pwm5 = 5;
-const int pwm6 = 6;
-const int pwm7 = 7;
-
-const int usb0 = 0; 
-const int usb1 = 1;
-const int usb2 = 2;
-
 void Robot::RobotInit() {
+  // Auto Mode Chooser
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  //Object Initialization
+  liftTop = new frc::DigitalInput(dio0);
+  liftMid = new frc::DigitalInput(dio1);
+  liftBottom = new frc::DigitalInput(dio2);
+
 }
 
 /**
