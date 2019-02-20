ed2d93ac1af41e58be5248c08e97efdd733b327d
PiNerd3 <solonh@gmail.com>
2019-02-20 08:30:10 -0800

    calibrating elevator heights for two of them (in comment)

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -83,8 +83,8 @@ class Robot : public frc::TimedRobot {
       {BALL_CARGO,      1 /* placeholder!!!! TODO */ },
       {HATCH_CARGO,     1 /* placeholder!!!! TODO */ },
       {HATCH_LOW,  -1000},
-      {HATCH_MID,   12000},
-      {HATCH_HIGH,  24200},
+      {HATCH_MID,   12000}, //9761
+      {HATCH_HIGH,  24200}, //20000
       {BALL_LOW,    4100 },
       {BALL_MID,    16800},
       {BALL_HIGH,   28550},
